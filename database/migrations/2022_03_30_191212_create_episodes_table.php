81ffb69f81eadf34f5a1ab8f31f34225f41fa606
tetracalibers <tomix05may@gmail.com>
2022-03-30 19:17:47 +0900

    episodesテーブルのマイグレーション

---
+++ database/migrations/2022_03_30_191212_create_episodes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->bigIncrements('episode_id');
+            $table->bigInteger('group_id');
+            $table->string('title', 100);
+            $table->string('catchphrase', 200);
+            $table->text('content');
+            $table->date('start_date');
+            $table->date('finish_date');
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+};
